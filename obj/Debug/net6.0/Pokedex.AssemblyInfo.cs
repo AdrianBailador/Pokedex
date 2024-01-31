71ad615c7238e02af64d060bff9f0f23c0f4b02d
Adrián Bailador Panero <adrianbailador@hotmail.com>
2024-01-31 22:24:23 +0000

    Show more

--- obj/Debug/net6.0/Pokedex.AssemblyInfo.cs
+++ obj/Debug/net6.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6da409dae2cbae78f38bacb179dcdde623eddef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229013a102ca53bf2a1dddaeae723aac7dba8e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
